639dea48e2241e03afbff49f319925ddd595827e
karthik-mohan23 <karthikmohan2310@gmail.com>
2023-07-17 22:16:47 +0530

    added restaurant cards

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,6 +1,7 @@
 import { useState, useEffect } from "react";
 import Banner from "../components/Banner";
 import RestaurantFilter from "../components/RestaurantFilter";
+import { RESTAURANT_IMAGE } from "../utils/links";
 
 const Home = () => {
   const [activeFilterButton, setActiveFilterButton] = useState(1);
@@ -34,6 +35,32 @@ const Home = () => {
           allRestaurants={allRestaurants}
         />
         <hr />
+        <div className="py-10 grid md:grid-cols-2 lg:grid-cols-4 gap-8">
+          {allRestaurants.map((restaurant) => {
+            return (
+              <div
+                key={restaurant.data.id}
+                className="outline-none hover:outline hover:outline-gray-200 hover:shadow-lg duration-300 w-[296px] px-5 py-5 cursor-pointer">
+                <div className="w-[254px]">
+                  <img
+                    src={RESTAURANT_IMAGE + restaurant.data.cloudinaryImageId}
+                    alt="restaurant"
+                    className="w-full block"
+                  />
+                </div>
+                <p>{restaurant.data.name}</p>
+                <p>{restaurant.data.cuisines.join(", ")}</p>
+                <div className="flex items-center justify-between">
+                  <p>{restaurant.data.avgRating}</p>
+                  <div>•</div>
+                  <p>{restaurant.data.deliveryTime}</p>
+                  <div>•</div>
+                  <p>{restaurant.data.costForTwoString}</p>
+                </div>
+              </div>
+            );
+          })}
+        </div>
       </section>
     </div>
   );
