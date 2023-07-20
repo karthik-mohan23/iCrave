561b6bae6dd3a00ddb20c499ace6bd7fbd63a5ca
karthik-mohan23 <karthikmohan2310@gmail.com>
2023-07-20 11:09:22 +0530

    made Banner component cleaner

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -1,35 +1,26 @@
+const bannerImage = [
+  "pneknawbadtvceqzwiep",
+  "lori508m4xps01tnywxf",
+  "zpkkdkmvlj5cuvqbc50t",
+  "s5ug2key6e2sptaxku5v",
+];
+
 const Banner = () => {
   return (
     <div className="bg-[#171a29]">
       <div className="w-[90%] max-w-7xl mx-auto py-9 flex items-center justify-between gap-16">
-        <div className="max-w-[260px] hover:scale-105 duration-700">
-          <img
-            src="https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_520,h_520/rng/md/carousel/production/pneknawbadtvceqzwiep"
-            alt="offers"
-            className="w-full block"
-          />
-        </div>
-        <div className="max-w-[260px] hover:scale-105 duration-700">
-          <img
-            src="https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_520,h_520/rng/md/carousel/production/lori508m4xps01tnywxf"
-            alt="offers"
-            className="w-full block"
-          />
-        </div>
-        <div className="max-w-[260px] hover:scale-105 duration-700">
-          <img
-            src="https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_520,h_520/rng/md/carousel/production/zpkkdkmvlj5cuvqbc50t"
-            alt="offers"
-            className="w-full block"
-          />
-        </div>
-        <div className="max-w-[260px] hover:scale-105 duration-700">
-          <img
-            src="https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_520,h_520/rng/md/carousel/production/s5ug2key6e2sptaxku5v"
-            alt="offers"
-            className="w-full block"
-          />
-        </div>
+        {bannerImage.map((image) => (
+          <div className="max-w-[260px] hover:scale-105 duration-700">
+            <img
+              src={
+                "https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_520,h_520/rng/md/carousel/production/" +
+                image
+              }
+              alt="offers"
+              className="w-full block"
+            />
+          </div>
+        ))}
       </div>
     </div>
   );
