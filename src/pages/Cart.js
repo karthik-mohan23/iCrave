457225d64d186e3041ffb7cb503a37bcd6856209
karthik-mohan23 <karthikmohan2310@gmail.com>
2023-08-01 16:20:38 +0530

    display items in Cart page

--- src/pages/Cart.js
+++ src/pages/Cart.js
@@ -1,5 +1,5 @@
 import { useSelector } from "react-redux";
-import { getTotalCartLength } from "../cart/cartSlice";
+import { getCart, getTotalCartLength } from "../cart/cartSlice";
 import EmptyCart from "../components/EmptyCart";
 
 
@@ -7,8 +7,64 @@ const Cart = () => {
 
 const cartLength = useSelector(getTotalCartLength)
 
-
+const cart = useSelector(getCart)
+console.log(cart);
 if (!cartLength) return <EmptyCart/>
-  return <div className="min-h-screen">Cart</div>;
+
+return <div className="min-h-screen">
+    <div className="w-[90%] max-w-7xl mx-auto flex justify-evenly flex-wrap-reverse lg:flex-nowrap py-10">
+      
+    <div>
+    <div className="grid grid-cols-4 mb-10 max-w-3xl">
+    <h2 >Item name</h2>
+      <p className="justify-self-center">Quantity</p>
+      <p className="justify-self-center">Price</p>
+      <button className="justify-self-end bg-red-500 text-white px-3 py-1 rounded-full">CLEAR ALL</button>
+    </div>
+{
+  cart.map(item => {
+    return (
+    <div key={item.id} className="grid grid-cols-4 my-4 max-w-3xl">
+      <h2 >{item.name}</h2>
+      <p className="justify-self-center self-center">{item.quantity}</p>
+      <p className="justify-self-center self-center">{item.totalPrice}</p>
+      <button className="text-xs justify-self-end self-center bg-yellow-300 px-4 py-2 rounded-full tracking-wide">REMOVE</button>
+    </div>
+    )
+  })
+}
+    </div>
+    
+    <div className="flex flex-col border border-black px-4 py-6 mb-10" >
+      <div> 
+        <h3 className="mb-3">Bill Details</h3>
+        <div className="flex justify-between items-center mb-2">
+            <p>Total Amount</p>
+            <p>$234</p>
+        </div>
+        <div className="flex justify-between items-center mb-2">
+            <p>Delivery fee</p>
+            <p>$234</p>
+        </div>
+        <div className="flex justify-between items-center mb-2">
+            <p>Platform fee</p>
+            <p>$234</p>
+        </div>
+        <div className="flex justify-between items-center mb-2">
+            <p className="w-3/4">GST and Restaurant Charges</p>
+            <p>$234</p>
+        </div>
+      </div>
+      <div className="h-[2px] bg-black"></div>
+      <div className="flex justify-between items-center py-4">
+          <h3>To Pay</h3>
+          <p>$234</p>
+      </div>
+      <button className="bg-black text-white py-1">Checkout</button>
+    </div>
+
+    </div>
+    
+  </div>;
 };
 export default Cart;
