5d5bb0b005bfb68cb5e0cc01ae80a00883025236
karthik-mohan23 <karthikmohan2310@gmail.com>
2023-09-16 22:42:49 +0530

    added background color for banner

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -11,19 +11,21 @@ const Banner = () => {
   // navigate
   const navigate = useNavigate();
   return (
-    <div className="w-full h-full overflow-x-scroll overflow-y-hidden  whitespace-nowrap scroll-smooth lg:mx-auto lg:flex lg:justify-between lg:max-w-7xl xl:overflow-x-hidden ">
-      {bannerImage.map((item) => (
-        <img
-          onClick={() => navigate("/offers")}
-          key={item}
-          className="w-[288px] inline-block p-2 cursor-pointer hover:scale-105 ease-in-out duration-300"
-          src={
-            "https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_520,h_520/rng/md/carousel/production/" +
-            item
-          }
-          alt="offers"
-        />
-      ))}
+    <div className="py-8 bg-gray-900">
+      <div className="w-full h-full overflow-x-scroll overflow-y-hidden  whitespace-nowrap scroll-smooth lg:mx-auto lg:flex lg:justify-between lg:max-w-7xl xl:overflow-x-hidden ">
+        {bannerImage.map((item) => (
+          <img
+            onClick={() => navigate("/offers")}
+            key={item}
+            className="w-[288px] inline-block p-2 cursor-pointer hover:scale-105 ease-in-out duration-300"
+            src={
+              "https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_520,h_520/rng/md/carousel/production/" +
+              item
+            }
+            alt="offers"
+          />
+        ))}
+      </div>
     </div>
   );
 };
